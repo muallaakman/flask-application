8a8325b09c5f2304eadc90f7e82c5a5cb985ed7a
Mualla <akmanmualla28@gmail.com>
2023-11-21 14:32:17 +0300

    second

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,8 +1,17 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    return 'hello world'
 
+@app.route('/second')
+def second():
+    return 'This is second page'
 
+@app.route('/third')
+def third():
+    return 'This is third page'
 
 
 
--- flask-02-Jinja_Template/jinja.py
+++ flask-02-Jinja_Template/jinja.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+@app.route("/")
+def head():
+    return render_template("index.html", number1=1234, number2=7896)
 
-
-
+@app.route("/cohort16")
+def number():
+    num1 = 12
+    num2 = 11
+    return render_template("body.html", value1=num1, value2=num2, sum=num1+num2)
 
 
 
